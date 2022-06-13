32c44758e7de523374baae6a250515105afecccd
David Ta <david.ta555@gmail.com>
2022-06-13 17:26:55 -0400

    index.css update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Technologies from "./components/technologies/Technologies";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Technologies />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
